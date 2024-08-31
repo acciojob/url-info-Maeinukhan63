cb294d4973181c5c1ebfcb47a51d598bf7bdd3e5
Maeinuddin khan <119691264+Maeinukhan63@users.noreply.github.com>
2024-08-31 13:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ const currentUrl = window.location.href;
+
+        // Calculate the length of the URL
+        const urlLength = currentUrl.length;
+
+        // Display the length of the URL using alert
+        alert(`The length of the URL is: ${urlLength}`);
